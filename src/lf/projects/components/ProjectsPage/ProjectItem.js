81ecc55206b4221cbadc284b636f6d84c2efca6f
Pouya Adib <pouyaadn@gmail.com>
2017-08-01 11:24:00 +0430

    show forget button

--- src/lf/projects/components/ProjectsPage/ProjectItem.js
+++ src/lf/projects/components/ProjectsPage/ProjectItem.js
@@ -1,14 +1,19 @@
 // @flow
 import React from 'react'
 import compose from 'ramda/src/compose'
+import css from './ProjectItem.css'
 
 type Props = {
   project: Object,
+  onClick: Function,
 }
 
 const ProjectItem = (props: Props) => {
   return (
-    <div>{props.project.bar}</div>
+    <div className={css.container}>
+      <div className={css.name}>{props.project.bar}</div>
+      <button className={css.forgetButton} onClick={props.onClick}>forget</button>
+    </div>
   )
 }
 
