99ed3d368673aa9477e30ac85b453ff459283f95
AriaMinaei <aria.minaei@gmail.com>
2017-08-04 17:01:01 +0200

    Fix the type of withRunSaga()

---
+++ src/shared/utils/sagas/withRunSaga.test.js
@@ -0,0 +1,28 @@
+// @flow
+import {type RunSagaFn} from './withRunSaga'
+
+describe('withRunSaga()', () => {
+  it('should work')
+
+  async function typeTests(){
+    declare var run: RunSagaFn
+    function* foo(a: string, b: number): Generator<*, boolean, *> {
+      yield null
+      return false
+    }
+
+    // $FlowExpectError
+    run(foo)
+
+    // $FlowExpectError
+    run(foo, 'foo')
+
+    run(foo, 'foo', 10)
+
+
+    const a: boolean = await run(foo, 'h', 1)
+
+    // $FlowExpectError
+    const b: string = await run(foo, 'h', 1)
+  }
+})
