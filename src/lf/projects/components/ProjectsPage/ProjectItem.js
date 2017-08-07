29d2bc467fd37c86b760c450cac4b1852b49bef1
Pouya Adib <pouyaadn@gmail.com>
2017-08-07 10:44:56 +0430

    take into account project's loadingState somehow!

--- src/lf/projects/components/ProjectsPage/ProjectItem.js
+++ src/lf/projects/components/ProjectsPage/ProjectItem.js
@@ -14,13 +14,17 @@ const ProjectItem = (props: Props) => {
   return (
     <div className={css.container}>
       <div className={css.projectInfo}>
-        {(props.projectDesc != null) && (props.projectDesc.loadingState === 'loaded')
-          ?
+        {(props.projectDesc != null && props.projectDesc.loadingState === 'loaded') &&
           <div className={css.title}>{props.projectDesc.name}</div>
-          :
+        }
+        {(!(props.projectDesc != null) 
+        || (props.projectDesc && props.projectDesc.loadingState === 'loading')) &&
           <div className={css.loading}>
             <span>L</span><span>o</span><span>a</span><span>d</span><span>i</span><span>n</span><span>g</span>
           </div>
+        }
+        {(props.projectDesc && props.projectDesc.loadingState === 'error') &&
+          <div className={css.title}>{props.projectDesc.message}</div>
         }
         <div title={props.path} className={css.path}>{props.path.slice(1).slice(0, -15)}</div>
       </div>
