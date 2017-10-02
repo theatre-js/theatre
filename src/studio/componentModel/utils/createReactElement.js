e686658dd38bb9e4f4c8833bded6e81324e44b3d
Pouya Adib <pouyaadn@gmail.com>
2017-10-02 10:24:13 +0330

    ignore react-render-hook flow errors

--- src/studio/componentModel/utils/createReactElement.js
+++ src/studio/componentModel/utils/createReactElement.js
@@ -2,7 +2,7 @@
 import * as React from 'react'
 import {type ComponentInstantiationDescriptor, type ComponentID} from '$studio/componentModel/types' // eslint-disable-line
 
-const createReactElement = (componentID: ComponentID, props: {}) => { // eslint-disable-line
+const createReactElement = (componentID: ComponentID, props: {children: *}) => { // eslint-disable-line
   return <div>{props.children}</div>
 }
 
