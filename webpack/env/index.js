45d770be7a25626a12b672b41e54b8396a3a4a26
AriaMinaei <aria.minaei@gmail.com>
2017-11-10 10:08:44 +0100

    test

--- webpack/env/index.js
+++ webpack/env/index.js
@@ -2,6 +2,8 @@
 import commitHash from 'exec-loader!./commitHash'
 import dotEnvFile from './dotEnvFile'
 
+console.log(commitHash)
+
 // Using `module.exports` instead of `export default` 'cause that's easier to configure in
 // webpack.ProvidePlugin
 module.exports = {COMMIT_HASH: commitHash, ...dotEnvFile}
