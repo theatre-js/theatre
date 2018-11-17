ece893363b593061147006df2fc3dc842f34d2b7
AriaMinaei <aria.minaei@gmail.com>
2018-11-17 15:22:37 +0100

    Disable devtools formatters (we don't need them anymore)

--- src/shared/DataVerse/index.tsx
+++ src/shared/DataVerse/index.tsx
@@ -1,19 +1,3 @@
-/* eslint-disable */
-
-/*:: export type * from './types' */
-
-// export {default as Ticker} from './Ticker'
-// /*:: export type * from './Ticker' */
-
-// import * as atoms from './atoms'
-// /*:: import * from './atoms' */
-// import * as derivations from './derivations'
-// /*:: import * from './derivations' */
-// import * as literals from './literals'
-// /*:: import * from './literals' */
-
-// export {atoms, derivations, literals}
-
-if ($env.NODE_ENV === 'development') {
-  require('./devtoolsFormatters/setup')
-}
+// if ($env.NODE_ENV === 'development') {
+//   require('./devtoolsFormatters/setup')
+// }
