91e005601c78563ce92e1eef17cfe8f69c59ac1c
AriaMinaei <aria.minaei@gmail.com>
2019-01-19 14:56:08 +0100

    Make the NumbeEditor signal its dragging mode more clearly

--- src/tl/ui/panels/AllInOnePanel/Right/items/StaticValueContainerItem/editors/NumberEditor.css.d.ts
+++ src/tl/ui/panels/AllInOnePanel/Right/items/StaticValueContainerItem/editors/NumberEditor.css.d.ts
@@ -1,6 +1,6 @@
 export const container: string;
 export const input: string;
+export const dragging: string;
 export const noFocus: string;
 export const value: string;
 export const editingViaKeyboard: string;
-export const dragging: string;
