df4f19c56f68787ecb0a1ef76dc229b1947299c3
AriaMinaei <aria.minaei@gmail.com>
2019-01-23 13:42:59 +0100

    Update examples/tl/1

--- examples/tl/1/index.ts
+++ examples/tl/1/index.ts
@@ -1,16 +1,49 @@
-// import {TypeOfTheatre} from '$src/tl/entries/index'
-// import {NativeObjectType} from '$tl/objects/objectTypes'
-// import * as css from './styles.css'
+import state from './state.json'
 
-// declare var Theatre: TypeOfTheatre
+import {TypeOfTheatre} from '$src/tl/entries/index'
 
-// const project = new Theatre.Project('Dotty')
-// const timeline = project.getTimeline('timeline')
+declare var Theatre: TypeOfTheatre
 
-// const canvas = document.createElement('canvas')
-// canvas.width = 100
+const project = Theatre.getProject("My project", {state})
+const timeline = project.getTimeline("Bounce")
 
-// document.body.appendChild(canvas)
-// const ctx = canvas.getContext('2d')
+const nativeObject = document.createElement('div')
+nativeObject.style.cssText = `
+  position: absolute;
+  left: 10vw;
+  bottom: 0;
+  width: 50px;
+  height: 50px;
+  transform-origin: center bottom;
+  background: #EEE;
+`
+document.body.appendChild(nativeObject)
 
-// import './old'
+// Create an object:
+const object = timeline.getObject(
+  // The name of the object is "The box":
+  "The box", 
+  // Leave a reference to the native object, which in this case is a div:
+  nativeObject,
+  // Define the properties of our object. In this case, we plan to animate the object by 
+  // moving it up and down, along the y axis. So we'll only have one prop for now:
+  {
+    props: {
+      // Call the prop "y":
+      y: {
+        // And the type of our "y" prop is number:
+        type: 'number'
+      },
+      stretch: {
+      type: 'number'
+    }
+    }
+  },
+)
+
+object.onValuesChange(newValues => {
+  const div = object.nativeObject
+    div.style.transform = `translateY(${- newValues.y}px) scaleY(${newValues.stretch}) scaleX(${1 / newValues.stretch})`
+})
+
+window.timeline = timeline
