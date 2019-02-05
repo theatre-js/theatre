ffba3c38a67aa13ddb1de2012361905230377a4f
AriaMinaei <aria.minaei@gmail.com>
2019-02-05 15:26:53 +0100

    Allow cold reads from Object.currentValues

--- src/tl/objects/TimelineInstanceObject.ts
+++ src/tl/objects/TimelineInstanceObject.ts
@@ -214,13 +214,10 @@ export default class TimelineInstanceObject {
   }
 
   get currentValues() {
-    if (!$env.tl.isCore) {
-      skipFindingColdDerivations()
-    }
-    const val = this._getValuesDerivation().getValueCold()
-    if (!$env.tl.isCore) {
-      endSkippingColdDerivations()
-    }
+    let val
+    // currently doesn't support cold reads
+    const untap = this._getValuesDerivation().tapImmediate(this._project.ticker, (v) => val = v)
+    untap()
     return val
   }
 }
