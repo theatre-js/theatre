d7442098626e71a63d2b95d57e352a6dce3e2e5d
AriaMinaei <aria.minaei@gmail.com>
2019-10-13 15:10:37 +0200

    Remove some unused types

--- src/shared/types.tsx
+++ src/shared/types.tsx
@@ -1,27 +1,9 @@
-import * as t from '$shared/ioTypes'
-import {difference} from '$shared/utils'
 export type GenericAction = {type: string; payload: mixed}
 
 export type ReduxReducer<State extends {}> = (
   s: undefined | State,
   action: GenericAction,
 ) => State
-
-export type ErrorsOf<T> = T extends (
-  ...args: $IntentionalAny[]
-) => Generator_<infer R>
-  ? R extends {errorType: string}
-    ? R
-    : never
-  : never
-
-export type ReturnOf<Fn> = Fn extends (
-  ...args: $IntentionalAny[]
-) => Generator_<infer R>
-  ? R
-  : never
-
-export type PromiseValue<P> = P extends Promise<infer R> ? R : never
 
 export type VoidFn = () => void
 
@@ -35,41 +17,6 @@ export const _any: $IntentionalAny = null
  * just write `expectType<V>(v)`
  */
 export const expectType = <T extends mixed>(v: T): T => v
-
-/**
- * An object of {list, byId} of type `type`, plus an invariant that
- * makes sure `list` only contains ids that exist in `byId`, and also
- * all `byId`s are listed in `list`
- */
-export const listAndById = <T extends mixed>(type: t.Type<T>, name: string) =>
-  t
-    .type(
-      {
-        list: t.array(t.string),
-        byId: t.record(t.string, type),
-      },
-      name,
-    )
-    .withRuntimeCheck(function listAndByIdInvariant(v: {
-      list: string[]
-      byId: Record<string, mixed>
-    }) {
-      const inListButNotInById = v.list.filter(id => !v.byId.hasOwnProperty(id))
-      if (inListButNotInById.length > 0) {
-        return [
-          `List ids ${JSON.stringify(inListButNotInById)} don't exist in byId.`,
-        ]
-      }
-
-      const diff = difference(Object.keys(v.byId), v.list)
-      if (diff.length > 0) {
-        return [
-          `Keys ${JSON.stringify(diff)} are present in byId but not in list`,
-        ]
-      }
-
-      return true
-    })
 
 /**
  * Takes a react component and returns the type of its props.
