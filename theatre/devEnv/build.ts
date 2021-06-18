b2d1f15446654cd56eb4ac54cbbe08a5c9d76944
Aria Minaei <aria.minaei@gmail.com>
2021-06-18 21:11:45 +0200

    (WIP) Switch the rest of the build setup to esbuild - done

--- theatre/devEnv/build.ts
+++ theatre/devEnv/build.ts
@@ -5,12 +5,15 @@ for (const which of ['core', 'studio']) {
   const pathToPackage = path.join(__dirname, '../', which)
   const esbuildConfig = {
     entryPoints: [path.join(pathToPackage, 'src/index.ts')],
-    target: ['firefox88'],
+    target: ['firefox88', 'chrome90'],
     loader: {'.png': 'file'},
     outfile: path.join(pathToPackage, 'dist/index.js'),
     bundle: true,
     sourcemap: true,
     define: definedGlobals,
+  }
+  if (which === 'core') {
+    esbuildConfig.target = ['firefox57', 'chrome58']
   }
   require('esbuild').build(esbuildConfig)
 }
