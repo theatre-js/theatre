c5b82dfbe18a86a976f756d31ca127ba1f3fee1e
Aria Minaei <aria.minaei@gmail.com>
2021-12-05 17:09:55 +0000

    WIP - Preset easing - 1

--- theatre/studio/src/panels/SequenceEditorPanel/DopeSheet/Right/BasicKeyframedTrack/KeyframeEditor/CurveEditorPopover/CurveEditorPopover.tsx
+++ theatre/studio/src/panels/SequenceEditorPanel/DopeSheet/Right/BasicKeyframedTrack/KeyframeEditor/CurveEditorPopover/CurveEditorPopover.tsx
@@ -6,13 +6,46 @@ import type {SequenceEditorPanelLayout} from '@theatre/studio/panels/SequenceEdi
 import getStudio from '@theatre/studio/getStudio'
 import type {CommitOrDiscard} from '@theatre/studio/StudioStore/StudioStore'
 import {propNameText} from '@theatre/studio/panels/DetailPanel/propEditors/utils/SingleRowPropEditor'
-import BasicStringInput from '@theatre/studio/uiComponents/form/BasicStringInput'
 import type KeyframeEditor from '@theatre/studio/panels/SequenceEditorPanel/DopeSheet/Right/BasicKeyframedTrack/KeyframeEditor/KeyframeEditor'
 import {round} from 'lodash-es'
 import type {Keyframe} from '@theatre/core/projects/store/types/SheetState_Historic'
 import type {$IntentionalAny} from '@theatre/shared/utils/types'
+import BasicAutocompleteInput from '@theatre/studio/uiComponents/form/BasicAutocompleteInput'
+import fuzzySort from 'fuzzysort'
 
-const greaterThanZero = (v: number) => isFinite(v) && v > 0
+const presets = [
+  {label: 'Linear', value: '0.5, 0.5, 0.5, 0.5'},
+  {label: 'Back In Out', value: '0.680, -0.550, 0.265, 1.550'},
+  {label: 'Back In', value: '0.600, -0.280, 0.735, 0.045'},
+  {label: 'Back Out', value: '0.175, 0.885, 0.320, 1.275'},
+  {label: 'Circ In Out', value: '0.785, 0.135, 0.150, 0.860'},
+  {label: 'Circ In', value: '0.600, 0.040, 0.980, 0.335'},
+  {label: 'Circ Out', value: '0.075, 0.820, 0.165, 1'},
+  {label: 'Cubic In Out', value: '0.645, 0.045, 0.355, 1'},
+  {label: 'Cubic In', value: '0.550, 0.055, 0.675, 0.190'},
+  {label: 'Cubic Out', value: '0.215, 0.610, 0.355, 1'},
+  {label: 'Ease Out In', value: '.42, 0, .58, 1'},
+  {label: 'Expo In Out', value: '1, 0, 0, 1'},
+  {label: 'Expo Out', value: '0.190, 1, 0.220, 1'},
+  {label: 'Quad In Out', value: '0.455, 0.030, 0.515, 0.955'},
+  {label: 'Quad In', value: '0.550, 0.085, 0.680, 0.530'},
+  {label: 'Quad Out', value: '0.250, 0.460, 0.450, 0.940'},
+  {label: 'Quart In Out', value: '0.770, 0, 0.175, 1'},
+  {label: 'Quart In', value: '0.895, 0.030, 0.685, 0.220'},
+  {label: 'Quart Out', value: '0.165, 0.840, 0.440, 1'},
+  {label: 'Quint In Out', value: '0.860, 0, 0.070, 1'},
+  {label: 'Quint In', value: '0.755, 0.050, 0.855, 0.060'},
+  {label: 'Quint Out', value: '0.230, 1, 0.320, 1'},
+  {label: 'Sine In Out', value: '0.445, 0.050, 0.550, 0.950'},
+  {label: 'Sine In', value: '0.470, 0, 0.745, 0.715'},
+  {label: 'Sine Out', value: '0.390, 0.575, 0.565, 1'},
+]
+
+const results = fuzzySort.go('out', presets, {key: 'label', allowTypo: false})
+console.log(results)
+results.forEach((r) => {
+  console.log(fuzzySort.highlight(r))
+})
 
 const Container = styled.div`
   display: flex;
@@ -121,7 +154,8 @@ const CurveEditorPopover: React.FC<
   return (
     <Container>
       <Label>cubic-bezier{'('}</Label>
-      <BasicStringInput
+      <BasicAutocompleteInput
+        autocompleteOptions={presets}
         value={cssCubicBezierString}
         {...fns}
         isValid={isValid}
