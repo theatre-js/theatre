b9a093e59d1d604803be38501033f0c98ed84005
Fülöp Kovács <kovacs.fulop@gmail.com>
2022-03-07 10:15:34 +0100

    [wip, fixup later] Move the FocusRangeStrip by dragging

--- theatre/studio/src/panels/SequenceEditorPanel/RightOverlay/FocusRange.tsx
+++ theatre/studio/src/panels/SequenceEditorPanel/RightOverlay/FocusRange.tsx
@@ -13,20 +13,64 @@ import {clamp} from 'lodash-es'
 import getStudio from '@theatre/studio/getStudio'
 import type {CommitOrDiscard} from '@theatre/studio/StudioStore/StudioStore'
 
+export const focusRangeTheme = {
+  active: {
+    backgroundColor: '#70a904',
+  },
+  inactive: {
+    backgroundColor: '#395209',
+  },
+}
+
+const focusRangeThumbWidth = 10
+
 const Handler = styled.div`
-  background-color: blue;
-  width: 20px;
+  content: ' ';
+  background-color: ${focusRangeTheme.active.backgroundColor};
+  width: ${focusRangeThumbWidth};
   height: 100%;
   position: absolute;
+  ${pointerEventsAutoInNormalMode};
+  left: -${focusRangeThumbWidth / 2}px;
+`
+
+const RangeStrip = styled.div`
+  /* background-color: ${focusRangeTheme.active.backgroundColor}; */
+  background-color: blue;
+  position: absolute;
+  height: 100%;
   cursor: ew-resize;
   ${pointerEventsAutoInNormalMode};
+  left: -${focusRangeThumbWidth / 2}px;
 `
+
+function clampRange(
+  start: number,
+  end: number,
+  minWidth: number,
+  maxWidth: number,
+): [number, number] {
+  let overflow = 0
+
+  if (start < minWidth) {
+    overflow = 0 - start
+  }
+
+  if (end > maxWidth) {
+    overflow = maxWidth - end
+  }
+
+  start += overflow
+  end += overflow
+
+  return [start, end]
+}
 
 const FocusRangeThumb: React.FC<{
   layoutP: Pointer<SequenceEditorPanelLayout>
   type: 'start' | 'end'
 }> = ({layoutP, type}) => {
-  const [startRef, startNode] = useRefAndState<HTMLElement | null>(null)
+  const [thumbRef, thumbNode] = useRefAndState<HTMLElement | null>(null)
 
   const existingRangeD = useMemo(
     () =>
@@ -99,11 +143,11 @@ const FocusRangeThumb: React.FC<{
         }
         tempTransaction = undefined
       },
-      lockCursorTo: 'ew-resize',
+      lockCursorTo: type === 'start' ? 'w-resize' : 'e-resize',
     }
   }, [sheet])
 
-  useDrag(startNode, gestureHandlers)
+  useDrag(thumbNode, gestureHandlers)
 
   return usePrism(() => {
     let existingRange = existingRangeD.getValue() || {
@@ -117,11 +161,135 @@ const FocusRangeThumb: React.FC<{
 
     return (
       <Handler
-        ref={startRef as $IntentionalAny}
-        style={{transform: `translate3d(${posInClippedSpace}px, 0, 0)`}}
+        ref={thumbRef as $IntentionalAny}
+        style={{
+          transform: `translate3d(${posInClippedSpace}px, 0, 0)`,
+          cursor: type === 'start' ? 'w-resize' : 'e-resize',
+          /* left:
+            type === 'start'
+              ? `${focusRangeThumbWidth / 2}px`
+              : `-${focusRangeThumbWidth / 2}px`, */
+          // left: 0,
+        }}
       />
     )
-  }, [layoutP, startRef, existingRangeD])
+  }, [layoutP, thumbRef, existingRangeD])
+}
+
+const FocusRangeStrip: React.FC<{
+  layoutP: Pointer<SequenceEditorPanelLayout>
+}> = ({layoutP}) => {
+  console.log('rerendered')
+  const existingRangeD = useMemo(
+    () =>
+      prism(() => {
+        const {projectId, sheetId} = val(layoutP.sheet).address
+        const existingRange = val(
+          getStudio().atomP.ahistoric.projects.stateByProjectId[projectId]
+            .stateBySheetId[sheetId].sequence.focusRange,
+        )
+        return existingRange
+      }),
+    [layoutP],
+  )
+
+  const sheet = val(layoutP.sheet)
+  let sequence = sheet.getSequence()
+
+  const [rangeSripRef, rangeStripNode] = useRefAndState<HTMLElement | null>(
+    null,
+  )
+
+  const gestureHandlers = useMemo((): Parameters<typeof useDrag>[1] => {
+    let scaledSpaceToUnitSpace: typeof layoutP.scaledSpace.toUnitSpace.$$__pointer_type
+    const defaultFocusRange = {start: 0, end: sequence.length}
+    let focusRange = existingRangeD.getValue()?.range || defaultFocusRange
+    let startPosBeforeDrag = focusRange.start
+    let endPosBeforeDrag = focusRange.end
+    let width = focusRange.end - focusRange.start
+    let tempTransaction: CommitOrDiscard | undefined
+    let dragHappened = false
+
+    return {
+      onDragStart() {
+        dragHappened = false
+        sequence = val(layoutP.sheet).getSequence()
+        startPosBeforeDrag =
+          existingRangeD.getValue()?.range.start || defaultFocusRange.start
+        endPosBeforeDrag =
+          existingRangeD.getValue()?.range.end || defaultFocusRange.end
+        scaledSpaceToUnitSpace = val(layoutP.scaledSpace.toUnitSpace)
+      },
+      onDrag(dx, _, event) {
+        dragHappened = true
+        focusRange = existingRangeD.getValue()?.range || defaultFocusRange
+        console.log(focusRange.end)
+
+        const deltaPos = scaledSpaceToUnitSpace(dx)
+
+        const [newStartPosition, newEndPosition] = clampRange(
+          startPosBeforeDrag + deltaPos,
+          endPosBeforeDrag + deltaPos,
+          0,
+          sequence.length,
+        )
+
+        if (tempTransaction) {
+          tempTransaction.discard()
+        }
+
+        tempTransaction = getStudio().tempTransaction(({stateEditors}) => {
+          stateEditors.studio.ahistoric.projects.stateByProjectId.stateBySheetId.sequence.focusRange.set(
+            {
+              ...sheet.address,
+              range: {
+                start: newStartPosition,
+                end: newEndPosition,
+              },
+            },
+          )
+        })
+      },
+      onDragEnd() {
+        if (dragHappened) {
+          if (tempTransaction) {
+            tempTransaction.commit()
+          }
+        } else {
+          if (tempTransaction) {
+            tempTransaction.discard()
+          }
+        }
+        tempTransaction = undefined
+      },
+      lockCursorTo: 'ew-resize',
+    }
+  }, [sheet])
+
+  useDrag(rangeStripNode, gestureHandlers)
+
+  return usePrism(() => {
+    let existingRange = existingRangeD.getValue() || {
+      range: {start: 0, end: sequence.length},
+    }
+
+    const startPosInClippedSpace = val(layoutP.clippedSpace.fromUnitSpace)(
+      existingRange.range.start,
+    )
+    const endPosInClippedSpace = val(layoutP.clippedSpace.fromUnitSpace)(
+      existingRange.range.end,
+    )
+
+    return (
+      <RangeStrip
+        ref={rangeSripRef as $IntentionalAny}
+        style={{
+          transform: `translate3d(${startPosInClippedSpace}px, 0, 0)`,
+          width: endPosInClippedSpace - startPosInClippedSpace,
+        }}
+      />
+    )
+  }, [layoutP, rangeStripNode, existingRangeD])
 }
 
 const FocusRange: React.FC<{
@@ -129,6 +297,7 @@ const FocusRange: React.FC<{
 }> = ({layoutP}) => {
   return (
     <>
+      <FocusRangeStrip layoutP={layoutP} />
       <FocusRangeThumb type="start" layoutP={layoutP} />
       <FocusRangeThumb type="end" layoutP={layoutP} />
     </>
--- theatre/studio/src/panels/SequenceEditorPanel/RightOverlay/TopStrip.tsx
+++ theatre/studio/src/panels/SequenceEditorPanel/RightOverlay/TopStrip.tsx
@@ -7,7 +7,7 @@ import StampsGrid from '@theatre/studio/panels/SequenceEditorPanel/FrameGrid/Sta
 import PanelDragZone from '@theatre/studio/panels/BasePanel/PanelDragZone'
 import {attributeNameThatLocksFramestamp} from '@theatre/studio/panels/SequenceEditorPanel/FrameStampPositionProvider'
 import {pointerEventsAutoInNormalMode} from '@theatre/studio/css'
-import FocusRange from './FocusRange'
+import FocusRange, {focusRangeTheme} from './FocusRange'
 
 export const topStripHeight = 20
 
@@ -30,10 +30,13 @@ const Container = styled(PanelDragZone)`
 
 const FocusRangeContainer = styled.div`
   position: absolute;
+  /* t */
+  op: ${topStripHeight}px;
   top: 0;
   left: 0;
   right: 0;
-  height: ${topStripHeight}px;
+  height: ${topStripHeight / 2}px;
+  background-color: ${focusRangeTheme.inactive.backgroundColor};
   box-sizing: border-box;
 `
 
