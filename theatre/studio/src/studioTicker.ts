173f8e47a5ca9792db4cd7d32514b6b55a63ce4f
Pete Feltham <dev@felthy.com>
2022-03-17 12:04:44 +1100

    Refactor default tickers to share code; prevent multiple animation frames being requested

--- theatre/studio/src/studioTicker.ts
+++ theatre/studio/src/studioTicker.ts
@@ -1,24 +1,7 @@
-import {Ticker} from '@theatre/dataverse'
+import {defaultTickerFactory} from '@theatre/dataverse'
 
-const studioTicker = new Ticker()
+const {ticker, enableDefaultTicker, disableDefaultTicker} =
+  defaultTickerFactory()
 
-export default studioTicker
-
-let defaultTickerEnabled = true
-
-export function enableDefaultTicker() {
-  if (!defaultTickerEnabled) {
-    defaultTickerEnabled = true
-    window.requestAnimationFrame(onAnimationFrame)
-  }
-}
-
-export function disableDefaultTicker() {
-  defaultTickerEnabled = false
-}
-
-const onAnimationFrame = (t: number) => {
-  studioTicker.tick(t)
-  if (defaultTickerEnabled) window.requestAnimationFrame(onAnimationFrame)
-}
-if (defaultTickerEnabled) window.requestAnimationFrame(onAnimationFrame)
+export default ticker
+export {enableDefaultTicker, disableDefaultTicker}
