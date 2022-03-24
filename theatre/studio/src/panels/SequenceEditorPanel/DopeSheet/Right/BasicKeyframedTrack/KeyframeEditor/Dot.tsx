0a28eecf74460965ed859bd6c3368885946ff4a5
vezwork <elliot@theatrejs.com>
2022-03-24 12:16:25 -0400

    Add connector copy keyframes and fix highlight left margin

--- theatre/studio/src/panels/SequenceEditorPanel/DopeSheet/Right/BasicKeyframedTrack/KeyframeEditor/Dot.tsx
+++ theatre/studio/src/panels/SequenceEditorPanel/DopeSheet/Right/BasicKeyframedTrack/KeyframeEditor/Dot.tsx
@@ -16,6 +16,7 @@ import {useLockFrameStampPosition} from '@theatre/studio/panels/SequenceEditorPa
 import {attributeNameThatLocksFramestamp} from '@theatre/studio/panels/SequenceEditorPanel/FrameStampPositionProvider'
 import {useCursorLock} from '@theatre/studio/uiComponents/PointerEventsHandler'
 import SnapCursor from './SnapCursor.svg'
+import selectedKeyframeIdsIfInSingleTrack from '@theatre/studio/panels/SequenceEditorPanel/DopeSheet/Right/BasicKeyframedTrack/selectedKeyframeIdsIfInSingleTrack'
 
 export const dotSize = 6
 const hitZoneSize = 12
@@ -276,23 +277,4 @@ function copyKeyFrameContextMenuItem(props: IProps, keyframeIds: string[]) {
       })
     },
   }
-}
-/**
- * @param selection - selection on the dope sheet, or undefined if there isn't a selection
- * @returns If the selection exists and contains one or more keyframes only in a single track,
- * then a list of those keyframe's ids; otherwise null
- */
-function selectedKeyframeIdsIfInSingleTrack(
-  selection: DopeSheetSelection | undefined,
-): string[] | null {
-  if (!selection) return null
-  const objectKeys = Object.keys(selection.byObjectKey)
-  if (objectKeys.length !== 1) return null
-  const object = selection.byObjectKey[objectKeys[0]]
-  if (!object) return null
-  const trackIds = Object.keys(object.byTrackId)
-  const firstTrack = object.byTrackId[trackIds[0]]
-  if (trackIds.length !== 1 && firstTrack) return null
-
-  return Object.keys(firstTrack!.byKeyframeId)
 }
