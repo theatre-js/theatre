3b3f7fcb04f1ad7c5bfbb7d72ae75811d1bb86e4
Aria Minaei <aria.minaei@gmail.com>
2022-04-09 11:57:52 +0200

    Small re-org of a test

--- theatre/core/src/sheetObjects/SheetObject.test.ts
+++ theatre/core/src/sheetObjects/SheetObject.test.ts
@@ -95,77 +95,79 @@ describe(`SheetObject`, () => {
     })
   })
 
-  test('it should support sequenced props', async () => {
-    const {objPublicAPI, sheet} = await setupTestSheet({
-      staticOverrides: {
-        byObject: {},
-      },
-      sequence: {
-        type: 'PositionalSequence',
-        length: 20,
-        subUnitsPerUnit: 30,
-        tracksByObject: {
-          obj: {
-            trackIdByPropPath: {
-              [encodePathToProp(['position', 'y'])]: asSequenceTrackId('1'),
-            },
-            trackData: {
-              '1': {
-                type: 'BasicKeyframedTrack',
-                keyframes: [
-                  {
-                    id: asKeyframeId('0'),
-                    position: 10,
-                    connectedRight: true,
-                    handles: [0.5, 0.5, 0.5, 0.5],
-                    value: 3,
-                  },
-                  {
-                    id: asKeyframeId('1'),
-                    position: 20,
-                    connectedRight: false,
-                    handles: [0.5, 0.5, 0.5, 0.5],
-                    value: 6,
-                  },
-                ],
+  describe(`sequenced overrides`, () => {
+    test('calculation of sequenced overrides', async () => {
+      const {objPublicAPI, sheet} = await setupTestSheet({
+        staticOverrides: {
+          byObject: {},
+        },
+        sequence: {
+          type: 'PositionalSequence',
+          length: 20,
+          subUnitsPerUnit: 30,
+          tracksByObject: {
+            obj: {
+              trackIdByPropPath: {
+                [encodePathToProp(['position', 'y'])]: asSequenceTrackId('1'),
+              },
+              trackData: {
+                '1': {
+                  type: 'BasicKeyframedTrack',
+                  keyframes: [
+                    {
+                      id: asKeyframeId('0'),
+                      position: 10,
+                      connectedRight: true,
+                      handles: [0.5, 0.5, 0.5, 0.5],
+                      value: 3,
+                    },
+                    {
+                      id: asKeyframeId('1'),
+                      position: 20,
+                      connectedRight: false,
+                      handles: [0.5, 0.5, 0.5, 0.5],
+                      value: 6,
+                    },
+                  ],
+                },
               },
             },
           },
         },
-      },
+      })
+
+      const seq = sheet.publicApi.sequence
+
+      const objValues = iterateOver(prism(() => objPublicAPI.value))
+
+      expect(seq.position).toEqual(0)
+
+      expect(objValues.next().value).toMatchObject({
+        position: {x: 0, y: 3, z: 0},
+      })
+
+      seq.position = 5
+      expect(seq.position).toEqual(5)
+      expect(objValues.next().value).toMatchObject({
+        position: {x: 0, y: 3, z: 0},
+      })
+
+      seq.position = 11
+      expect(objValues.next().value).toMatchObject({
+        position: {x: 0, y: 3.29999747758308, z: 0},
+      })
+
+      seq.position = 15
+      expect(objValues.next().value).toMatchObject({
+        position: {x: 0, y: 4.5, z: 0},
+      })
+
+      seq.position = 22
+      expect(objValues.next().value).toMatchObject({
+        position: {x: 0, y: 6, z: 0},
+      })
+
+      objValues.return()
     })
-
-    const seq = sheet.publicApi.sequence
-
-    const objValues = iterateOver(prism(() => objPublicAPI.value))
-
-    expect(seq.position).toEqual(0)
-
-    expect(objValues.next().value).toMatchObject({
-      position: {x: 0, y: 3, z: 0},
-    })
-
-    seq.position = 5
-    expect(seq.position).toEqual(5)
-    expect(objValues.next().value).toMatchObject({
-      position: {x: 0, y: 3, z: 0},
-    })
-
-    seq.position = 11
-    expect(objValues.next().value).toMatchObject({
-      position: {x: 0, y: 3.29999747758308, z: 0},
-    })
-
-    seq.position = 15
-    expect(objValues.next().value).toMatchObject({
-      position: {x: 0, y: 4.5, z: 0},
-    })
-
-    seq.position = 22
-    expect(objValues.next().value).toMatchObject({
-      position: {x: 0, y: 6, z: 0},
-    })
-
-    objValues.return()
   })
 })
