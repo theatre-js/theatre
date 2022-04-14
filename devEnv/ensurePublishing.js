c442be66e413e8e9c8d6db10c9af33b14fc9b58f
Fülöp Kovács <kovacs.fulop@gmail.com>
2022-04-14 20:33:31 +0200

    Test if `yalc` can be used in the CI for the build tests

--- devEnv/ensurePublishing.js
+++ devEnv/ensurePublishing.js
@@ -1,4 +1,7 @@
-if (process.env.THEATRE_IS_PUBLISHING !== 'true') {
+if (
+  process.env.THEATRE_IS_PUBLISHING !== 'true' &&
+  process.env.USING_YALC !== 'true'
+) {
   throw Error(
     `This script may run only when the "release" command in monorepo's root is running.`,
   )
