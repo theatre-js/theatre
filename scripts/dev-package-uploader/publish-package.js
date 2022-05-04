f7cce2ec3f0e71c862049d4b1653e1567c7446eb
Fülöp Kovács <kovacs.fulop@gmail.com>
2022-05-04 11:25:32 +0200

    Check if the tarball exists before publishing it

--- scripts/dev-package-uploader/publish-package.js
+++ scripts/dev-package-uploader/publish-package.js
@@ -2,6 +2,8 @@ const {PutObjectCommand, S3Client} = require('@aws-sdk/client-s3')
 const fs = require('fs')
 const path = require('path')
 
+const TARBALL_PATH = process.env.TARBALL_PATH
+
 const s3Client = new S3Client({
   endpoint: 'https://fra1.digitaloceanspaces.com', // Find your endpoint in the control panel, under Settings. Prepend "https://".
   region: 'fra1', // Must be "us-east-1" when creating new Spaces. Otherwise, use the region in your endpoint (e.g. nyc3).
@@ -11,11 +13,22 @@ const s3Client = new S3Client({
   },
 })
 
-const fileStream = fs.readFileSync(process.env.TARBALL_PATH)
+// Check if the tarball exists
+try {
+  fs.accessSync(TARBALL_PATH)
+} catch (error) {
+  if (error.code === 'ENOENT') {
+    throw new Error(`\x1b[31mFile does not exist: ${TARBALL_PATH}\x1b[0m`)
+  } else {
+    throw error
+  }
+}
+
+const fileStream = fs.readFileSync(TARBALL_PATH)
 
 const s3Params = {
   Bucket: process.env.BUCKET, // The path to the directory you want to upload the object to, starting with your Space name.
-  Key: path.basename(process.env.TARBALL_PATH), // Object key, referenced whenever you want to access this file later.
+  Key: path.basename(TARBALL_PATH), // Object key, referenced whenever you want to access this file later.
   // Body: "Hello, World!", // The object's contents. This variable is an object, not a string.
   ACL: 'public-read', // Defines ACL permissions, such as private or public.
   Body: fileStream,
