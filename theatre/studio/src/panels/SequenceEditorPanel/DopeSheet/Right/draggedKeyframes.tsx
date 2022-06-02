abc203d1760efe64d31ebebe10dcf00e5937d69e
Fülöp Kovács <kovacs.fulop@gmail.com>
2022-06-02 15:17:45 +0200

    [fixup later] Add some documentation to `draggedKeyframes.tsx`

--- theatre/studio/src/panels/SequenceEditorPanel/DopeSheet/Right/draggedKeyframes.tsx
+++ theatre/studio/src/panels/SequenceEditorPanel/DopeSheet/Right/draggedKeyframes.tsx
@@ -3,20 +3,39 @@ import type {KeyframeId} from '@theatre/shared/utils/ids'
 
 export type IDraggedKeyframes = Set<KeyframeId>
 
+/**
+ * The ids of the keyframes that are being dragged
+ */
 export const draggedKeyframesB: Box<Set<KeyframeId>> = new Box(new Set())
 
-export const draggedKeyframesUtils = {
-  resetDraggedKeyframes() {
-    draggedKeyframesB.set(new Set())
+/**
+ * Utils for the set of the dragged keyframes
+ */
+type IDraggedKeyframesUtils = {
+  resetDraggedKeyframes: () => void
+  addKeyframeIdsToDragged: (keyframeIds: KeyframeId[]) => void
+  removeKeyframeIdsFromDragged: (keyframeIds: KeyframeId[]) => void
+}
+
+/**
+ * See {@link IDraggedKeyframesUtils} for the docs
+ */
+export const draggedKeyframesUtils: IDraggedKeyframesUtils = {
+  resetDraggedKeyframes(): void {
+    const originalKeyframes = draggedKeyframesB.get()
+    if (originalKeyframes.size > 0) {
+      draggedKeyframesB.set(new Set())
+    }
   },
-  addKeyframeIdsToDragged(keyframeIds: KeyframeId[]) {
+  addKeyframeIdsToDragged(keyframeIds: KeyframeId[]): void {
     const originalDraggedKeyframes = draggedKeyframesB.get()
     for (let kfId of keyframeIds) {
       originalDraggedKeyframes.add(kfId)
     }
     draggedKeyframesB.set(originalDraggedKeyframes)
   },
-  removeKeyframeIdsFromDragged(keyframeIds: KeyframeId[]) {
+  // Q: Are we using this anywhere?
+  removeKeyframeIdsFromDragged(keyframeIds: KeyframeId[]): void {
     const updatedDraggedKeyframes: Set<KeyframeId> = draggedKeyframesB.get()
     for (let kfId of keyframeIds) {
       updatedDraggedKeyframes.delete(kfId)
