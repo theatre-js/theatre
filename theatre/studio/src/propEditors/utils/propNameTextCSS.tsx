dc7e5a1128d1be2ff9c6e86ce8222771009e4827
Aria Minaei <aria.minaei@gmail.com>
2022-06-13 15:00:21 +0200

    Use text color rather than bg to highlight hovered props

--- theatre/studio/src/propEditors/utils/propNameTextCSS.tsx
+++ theatre/studio/src/propEditors/utils/propNameTextCSS.tsx
@@ -1,8 +1,9 @@
+import type {PropHighlighted} from '@theatre/studio/panels/SequenceEditorPanel/whatPropIsHighlighted'
 import {css} from 'styled-components'
 
-export const propNameTextCSS = css`
+export const propNameTextCSS = css<{isHighlighted?: PropHighlighted}>`
   font-weight: 300;
   font-size: 11px;
-  color: #919191;
+  color: ${(props) => (props.isHighlighted === 'self' ? '#CCC' : '#919191')};
   text-shadow: 0.5px 0.5px 2px rgba(0, 0, 0, 0.3);
 `
