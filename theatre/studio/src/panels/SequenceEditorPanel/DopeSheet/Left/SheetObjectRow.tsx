6ede3f036c087d9247c316ed7fb723f309bdb194
Aria Minaei <aria.minaei@gmail.com>
2022-06-13 15:00:51 +0200

    Remove unused `usePrism()`

--- theatre/studio/src/panels/SequenceEditorPanel/DopeSheet/Left/SheetObjectRow.tsx
+++ theatre/studio/src/panels/SequenceEditorPanel/DopeSheet/Left/SheetObjectRow.tsx
@@ -1,5 +1,4 @@
 import type {SequenceEditorTree_SheetObject} from '@theatre/studio/panels/SequenceEditorPanel/layout/tree'
-import {usePrism} from '@theatre/react'
 import React from 'react'
 import AnyCompositeRow from './AnyCompositeRow'
 import {decideRowByPropType} from './PropWithChildrenRow'
@@ -9,28 +8,26 @@ import getStudio from '@theatre/studio/getStudio'
 const LeftSheetObjectRow: React.VFC<{
   leaf: SequenceEditorTree_SheetObject
 }> = ({leaf}) => {
-  return usePrism(() => {
-    return (
-      <AnyCompositeRow
-        leaf={leaf}
-        label={leaf.sheetObject.address.objectKey}
-        isCollapsed={leaf.isCollapsed}
-        toggleSelect={() => {
-          // set selection to this sheet object on click
-          getStudio().transaction(({stateEditors}) => {
-            stateEditors.studio.historic.panels.outline.selection.set([
-              leaf.sheetObject,
-            ])
-          })
-        }}
-        toggleCollapsed={() =>
-          setCollapsedSheetObjectOrCompoundProp(!leaf.isCollapsed, leaf)
-        }
-      >
-        {leaf.children.map((leaf) => decideRowByPropType(leaf))}
-      </AnyCompositeRow>
-    )
-  }, [leaf])
+  return (
+    <AnyCompositeRow
+      leaf={leaf}
+      label={leaf.sheetObject.address.objectKey}
+      isCollapsed={leaf.isCollapsed}
+      toggleSelect={() => {
+        // set selection to this sheet object on click
+        getStudio().transaction(({stateEditors}) => {
+          stateEditors.studio.historic.panels.outline.selection.set([
+            leaf.sheetObject,
+          ])
+        })
+      }}
+      toggleCollapsed={() =>
+        setCollapsedSheetObjectOrCompoundProp(!leaf.isCollapsed, leaf)
+      }
+    >
+      {leaf.children.map((leaf) => decideRowByPropType(leaf))}
+    </AnyCompositeRow>
+  )
 }
 
 export default LeftSheetObjectRow
