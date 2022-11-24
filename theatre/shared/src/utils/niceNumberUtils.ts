7899f8a965fa041e65f07329f5088c4135a976b8
Aria Minaei <aria.minaei@gmail.com>
2022-11-24 14:17:22 +0100

    Document `nicestNumberBetween()`

--- theatre/shared/src/utils/niceNumberUtils.ts
+++ theatre/shared/src/utils/niceNumberUtils.ts
@@ -2,38 +2,39 @@ import padEnd from 'lodash-es/padEnd'
 import logger from '@theatre/shared/logger'
 
 /**
- * Returns the _roundest_ number `c`, such that `a <= c <= b`. This is useful
- * when a number value is beinged "nudged" by a user, and we want to avoid setting
- * it to weird value like `101.1239293814314`, when we know that the user probably just meant `100`.
+ * Returns the _aesthetically pleasing_ (aka "nicest") number `c`, such that `a <= c <= b`.
+ * This is useful when a numeric value is being "nudged" by the user (e.g. dragged via mouse pointer),
+ * and we want to avoid setting it to weird value like `101.1239293814314`, when we know that the user
+ * probably just meant `100`.
  *
  * Examples
  * ```ts
- * roundestNumberBetween(0.1111111123, 0.2943439448) // 0.25
- * roundestNumberBetween(0.19, 0.23) // 0.2
- * roundestNumberBetween(1921, 1998) // 1950
- * roundestNumberBetween(10, 110) // 100
- * // There are many more examples at `./numberRoundingUtils.test.ts`
+ * nicestNumberBetween(0.1111111123, 0.2943439448) // 0.25
+ * nicestNumberBetween(0.19, 0.23) // 0.2
+ * nicestNumberBetween(1921, 1998) // 1950
+ * nicestNumberBetween(10, 110) // 100
+ * // There are more examples at `./niceNumberUtils.test.ts`
  * ```
  */
-export function roundestNumberBetween(_a: number, _b: number): number {
+export function nicestNumberBetween(_a: number, _b: number): number {
   if (_b < _a) {
-    return roundestNumberBetween(_b, _a)
+    return nicestNumberBetween(_b, _a)
   }
 
   if (_a < 0 && _b < 0) {
-    return noMinusZero(roundestNumberBetween(-_b, -_a) * -1)
+    return noMinusZero(nicestNumberBetween(-_b, -_a) * -1)
   }
 
   if (_a <= 0 && _b >= 0) return 0
 
   const aCeiling = Math.ceil(_a)
   if (aCeiling <= _b) {
-    return roundestIntegerBetween(aCeiling, Math.floor(_b))
+    return nicestIntegerBetween(aCeiling, Math.floor(_b))
   } else {
     const [a, b] = [_a, _b]
     const integer = Math.floor(a)
 
-    return integer + roundestFloat(a - integer, b - integer)
+    return integer + nicestFloatBetween(a - integer, b - integer)
   }
 }
 
@@ -41,7 +42,7 @@ export function roundestNumberBetween(_a: number, _b: number): number {
 const halvesAndQuartiles = [5, 2.5, 7.5]
 const multipliersWithoutQuartiles = [5, 2, 4, 6, 8, 1, 3, 7, 9]
 
-export function roundestIntegerBetween(
+export function nicestIntegerBetween(
   _a: number,
   _b: number,
   decimalsAllowed: boolean = true,
@@ -138,9 +139,11 @@ export const stringifyNumber = (n: number): string => {
 }
 
 /**
+ * The float-specific version of {@link nicestNumberBetween}.
+ *
  * it is expected that both args are 0 \< arg \< 1
  */
-export const roundestFloat = (a: number, b: number): number => {
+export const nicestFloatBetween = (a: number, b: number): number => {
   const inString = {
     a: stringifyNumber(a),
     b: stringifyNumber(b),
@@ -173,14 +176,15 @@ export const roundestFloat = (a: number, b: number): number => {
     b: padEnd(withoutInteger.b, maxNumberOfDecimals, '0'),
   }
 
-  const roundestInt = roundestIntegerBetween(
+  const mostAestheticInt = nicestIntegerBetween(
     parseInt(withPaddedDecimals.a, 10) * Math.pow(10, maxNumberOfLeadingZeros),
     parseInt(withPaddedDecimals.b, 10) * Math.pow(10, maxNumberOfLeadingZeros),
     true,
   )
 
   return toPrecision(
-    roundestInt / Math.pow(10, maxNumberOfLeadingZeros + maxNumberOfDecimals),
+    mostAestheticInt /
+      Math.pow(10, maxNumberOfLeadingZeros + maxNumberOfDecimals),
   )
 }
 
