bd2dca3d9a7b018b767cb315e9ca2a0b243a5017
Andrew Prifer <andrew.prifer@gmail.com>
2023-01-12 20:35:20 +0100

    Add DualSense.js copyright notice

--- packages/dualsense-bindings/src/ds-hid.ts
+++ packages/dualsense-bindings/src/ds-hid.ts
@@ -1,3 +1,31 @@
+/*
+Based on https://github.com/equalent/DualSense.js by Andrey Tsurkan, with the
+addition of gyro and accelerometer support, and a virtual "attitude" sensor that
+fuses data from the gyro and accelerometer to give the controller's orientation.
+
+MIT License
+
+Copyright (c) 2022 Andrey Tsurkan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
 import AHRS from 'ahrs'
 import crc32 from 'crc/crc32'
 import {Buffer} from 'buffer/'
